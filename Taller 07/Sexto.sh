7a8f51aa1ce6f198690c1c91ed441e08bd48ff6a
Carlos Castañeda <83093835+Ankarkis@users.noreply.github.com>
2023-05-07 00:54:08 -0500

    Create Sexto.sh

---
+++ Taller 07/Sexto.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo .txt:
+
+find . -type f -name "*.txt"
+
+echo ------------------------------
+
+echo .png:
+
+find . -type f -name "*.png"
+
+echo ------------------------------
+
+echo .jpg:
+
+find . -type f -name "*.jpg"
